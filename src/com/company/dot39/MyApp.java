67cd92ad692ee33600a5b242a2319e28cd9e2a0c
Yuta Egi <yuta5196@gmail.com>
2018-07-29 21:41:25 +0900

    added dotinstall #38

---
+++ src/com/company/dot39/MyApp.java
@@ -0,0 +1,31 @@
+package com.company.dot39;
+
+import java.util.*;
+
+public class MyApp {
+
+    public static void main(String[] args) {
+
+        //ArrayList<Integer> sales = new ArrayList<>();
+        List<Integer> sales = new ArrayList<>();
+
+        sales.add(10);
+        sales.add(20);
+        sales.add(30);
+
+        for (int i = 0; i < sales.size(); i++){
+            System.out.println(sales.get(i));
+
+        }
+
+        sales.set(0,100);
+        sales.remove(2);
+
+        for (Integer sale : sales){
+            System.out.println(sale);
+
+        }
+
+    }
+
+}
